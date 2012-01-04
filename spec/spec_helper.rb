53f3301311d74cf938a7becefb2a89919a499582
Akshay Rawat <projects@akshay.cc>
2012-01-05 01:29:25 +0530

    model spec fixes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 
+Dir[Rails.root.join("spec/examples/**/*.rb")].each {|f| require f}
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 Dir[Rails.root.join("spec/factories/**/*.rb")].each {|f| require f}
 
