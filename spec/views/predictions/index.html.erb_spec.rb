a1cd9fcc288cd6c1cfc11c8a348fcd7c074d6277
Akshay Rawat <projects@akshay.cc>
2012-01-06 17:57:04 +0530

    prediction/index view spec fixes

--- spec/views/predictions/index.html.erb_spec.rb
+++ spec/views/predictions/index.html.erb_spec.rb
@@ -1,33 +1,25 @@
 require 'spec_helper'
 
-describe 'An index of predictions' do
-  
-  before do
-    assigns[:predictions] = []
-    assigns[:statistics] = Statistics.new([])
-    template.stub!(:show_statistics?).and_return(false)
-    template.stub!(:current_user).and_return User.new
-  end
+describe 'predictions/index.html.erb' do
 
-  def render_view
-    render 'predictions/index'
+  before do
+    assign(:predictions, [])
+    assign(:statistics, Statistics.new([]))
+    view.stub!(:show_statistics?).and_return(false)
+    view.stub!(:current_user).and_return User.new
   end
 
   it 'should render without errors' do
-    render_view
-    response.should be_success
+    render
+    rendered.should_not be_blank
   end
 
   it 'should render predictions' do
-    prediction = Prediction.new
-    prediction.stub!(:to_param).and_return(437)
-    prediction.save(false)
-    prediction.stub!(:deadline).and_return(Time.now)
-    prediction.stub!(:created_at).and_return(Time.now)
-    prediction.stub!(:mean_confidence).and_return(10)
-    assigns[:predictions] = [prediction]
-    render_view
-    response.should have_tag('a[href=?]', prediction_path(prediction))
+    prediction = create_valid_prediction
+    assign(:predictions, [prediction])
+
+    render
+    rendered.should have_selector("a[href='#{prediction_path(prediction)}']")
   end
-  
+
 end
