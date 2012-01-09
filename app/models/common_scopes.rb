e7c073d03f1151d74731214a3145a702d0d08a34
Akshay Rawat <projects@akshay.cc>
2012-01-09 17:51:15 +0530

    use AR#limit, instead of custom implementation.closes #3.

--- app/models/common_scopes.rb
+++ app/models/common_scopes.rb
@@ -1,6 +1,5 @@
 module CommonScopes
   def self.included(base)
-    base.scope :limit, lambda { |*args| {:limit => args.first || 3} }
     base.scope :sort, lambda { |*args| {:order => "#{args.first || :created_at}"} }
     base.scope :rsort, lambda { |*args| {:order => "#{args.first || :created_at} DESC"} }
   end
--- config/initializers/array_ext.rb
+++ config/initializers/array_ext.rb
@@ -2,6 +2,4 @@ class Array
   def rsort(field)
     sort {|a,b| b.send(field) <=> a.send(field)}
   end
-  
-  alias_method :limit, :first
 end
