f4f4abb03b0c9aa3cf2a948b9731bf6365333c02
Akshay Rawat <projects@akshay.cc>
2012-01-09 19:55:20 +0530

    marked helper generated html fragment html_safe

--- app/helpers/deadline_helper.rb
+++ app/helpers/deadline_helper.rb
@@ -2,22 +2,23 @@ module DeadlineHelper
   def deadline_notification_info(deadline_notification)
     set_your_email = link_to "set your email", settings_user_path(deadline_notification.user)
     if deadline_notification.sent?
-      "Your email notification has been sent."
+      notification_text = "Your email notification has been sent."
     elsif deadline_notification.sendable?
-      "Your email notification will be sent soon."
+      notification_text = "Your email notification will be sent soon."
     elsif deadline_notification.due_for_judgement? and deadline_notification.enabled?
-      "To receive your notifications, #{set_your_email}"
+      notification_text = "To receive your notifications, #{set_your_email}"
     elsif deadline_notification.deadline < Time.current and deadline_notification.enabled?
-      "You would have been notified #{show_time(deadline_notification.deadline)}, but it was already judged."
+      notification_text = "You would have been notified #{show_time(deadline_notification.deadline)}, but it was already judged."
     else
       time = show_time(deadline_notification.deadline)
       will_would = deadline_notification.enabled ? 'will be' : 'would be'
       will_would = 'would have been' if deadline_notification.overdue?
       if deadline_notification.has_email?
-        "You #{will_would} notified #{time}."
+        notification_text = "You #{will_would} notified #{time}."
       else
-        "You #{will_would} notified #{time} if you #{set_your_email}."
+        notification_text = "You #{will_would} notified #{time} if you #{set_your_email}."
       end
+      notification_text.html_safe
     end
   end
   
