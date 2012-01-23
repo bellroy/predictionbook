9728cde9d1ee9d6185233cffa97d5cc3158d3fd5
Glenn Francis Murray <glenn@metonymous.com>
2012-01-24 10:14:53 +1100

    Set hostname and port

--- config/initializers/set_deliverer_hostname.rb
+++ config/initializers/set_deliverer_hostname.rb
@@ -1,2 +1,3 @@
 # Overridden when deployed by capistrano with deploy:set_deliverer_hostname
-Deliverer.default_url_options[:host] = 'localhost:3000'
+Deliverer.default_url_options[:host] = 'localhost'
+Deliverer.default_url_options[:port] = '3000'
