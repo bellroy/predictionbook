dea006f245d1a7674257f089c39e1546ef946687
Tom <tom@trikeapps.com>
2012-04-12 14:18:53 +1000

    Use ec2 instance for staging.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,6 +2,7 @@ set :domains, %w(staging.predictionbook.trikeapps.com)
 set :rails_env, 'staging'
 set :branch, "master"
 
-role :app,    'poppy.trikeapps.com'
-role :web,    'poppy.trikeapps.com'
-role :db,     'poppy.trikeapps.com', :primary => true
+role :app,    'staging.steak.trikeapps.com'
+role :web,    'staging.steak.trikeapps.com'
+role :db,     'staging.steak.trikeapps.com', :primary => true
+
