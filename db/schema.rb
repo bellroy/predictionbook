b9bf653b2af8f1c0dbc5fcb65c49dca6678ac13f
Tom <tom@trikeapps.com>
2012-05-07 13:49:20 +1000

    Add admin flag to user model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20080924030037) do
+ActiveRecord::Schema.define(:version => 20120507034756) do
 
   create_table "judgements", :force => true do |t|
     t.integer  "prediction_id"
@@ -92,6 +92,7 @@ ActiveRecord::Schema.define(:version => 20080924030037) do
     t.datetime "updated_at"
     t.string   "timezone"
     t.boolean  "private_default",                         :default => false
+    t.boolean  "admin",                                   :default => false, :null => false
   end
 
   add_index "users", ["login"], :name => "index_users_on_login", :unique => true
