5c8c51e520202e4374e45ed63fa19db40a8db6a0
Tom <tom@trikeapps.com>
2012-05-08 13:18:53 +1000

    Don't load the environment on precompilation.

--- config/application.rb
+++ config/application.rb
@@ -47,6 +47,7 @@ module PredictionBook2
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
