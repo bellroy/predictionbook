5b3ad8ebc24cc41891febebe18e81b12ddad18d7
Tom <tom@trikeapps.com>
2012-05-11 11:29:09 +1000

    Make sure we have the correct path for the crontab.

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,6 +3,8 @@
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
 
+env :PATH, "/usr/local/bin:/usr/bin:/bin"
+
 if environment == "production"
   every 5.minutes do
     rake "send_email_notifications"
