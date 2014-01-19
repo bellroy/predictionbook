0082608afec67fa291a9057e582355d2fc85ef5f
Philip Hazelden <philip.hazelden@gmail.com>
2014-01-19 22:47:53 +0000

    Report scores for credence answers.
    Change 100% to 99% on answer pages because 100% was a dumb brainfart and
    breaks the scoring rule.

--- app/controllers/credence_controller.rb
+++ app/controllers/credence_controller.rb
@@ -2,15 +2,21 @@ class CredenceController < ApplicationController
   def go
     @title = "Credence game"
     @question = CredenceQuestion.new
-
-    session[:correct_index] = @question.correct_index
+    session[:question] = @question
   end
 
   def answer
     @title = "Credence game"
-    @question = CredenceQuestion.new
 
+    question = session[:question]
     given_answer = params.has_key?('submit-0') ? 0 : 1
-    @correct = given_answer == session[:correct_index]
+    submit_name = "submit-#{given_answer}"
+    credence = params[submit_name].to_i
+
+    @correct = given_answer == question.correct_index
+    @score = question.score_answer(given_answer, credence)
+
+    @question = CredenceQuestion.new
+    session[:question] = @question
   end
 end
