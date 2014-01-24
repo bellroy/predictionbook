20e7febe75782a4d8b596ba447bdc0b07fcc819f
Philip Hazelden <philip.hazelden@gmail.com>
2014-01-24 19:45:34 +0000

    Revert "Change to ActiveRecordStore sessions."
    This reverts commit 1d1e8035b66442a89bfb6efa84d527c08bb68d98.
    
    Spoke to Wes at Trike. He gave me reasons to stick to cookie store (it's
    what Trike always uses, principle of least surprise), and ways to not
    need active record store (stick stuff in the model, obvious in
    hindsight).

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery :secret => 'c4ec086ac06ce802c8f49e28cc1e8943'
+  protect_from_forgery # :secret => 'c4ec086ac06ce802c8f49e28cc1e8943'
   
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140111164816) do
+ActiveRecord::Schema.define(:version => 20120507034756) do
 
   create_table "judgements", :force => true do |t|
     t.integer  "prediction_id"
@@ -79,16 +79,6 @@ ActiveRecord::Schema.define(:version => 20140111164816) do
 
   add_index "responses", ["prediction_id"], :name => "index_responses_on_prediction_id"
   add_index "responses", ["user_id"], :name => "index_responses_on_user_id"
-
-  create_table "sessions", :force => true do |t|
-    t.string   "session_id", :null => false
-    t.text     "data"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
-  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "users", :force => true do |t|
     t.string   "login"
