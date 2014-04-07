a77b60bfc62ee11c49bc476c76861ef6ed4b1b48
Ren Wenshan <wenshan@trikeapps.com>
2014-04-07 17:12:31 +1000

    Remove trailing whitespace

--- spec/controllers/responses_controller_spec.rb
+++ spec/controllers/responses_controller_spec.rb
@@ -8,7 +8,7 @@ describe ResponsesController do
   before(:each) do
     controller.stub!(:set_timezone)
   end
-  
+
   describe 'creating a new response' do
     before(:each) do
       @wager = mock_model(Response, :save => true)
@@ -20,35 +20,35 @@ describe ResponsesController do
       Prediction.stub!(:find).and_return(@prediction)
       controller.stub!(:logged_in?).and_return(true)
     end
-    
+
     it 'should require the user to be logged in' do
       controller.stub!(:logged_in?).and_return(false)
       post_response
       response.should redirect_to(login_path)
     end
-    
+
     it 'should create a response on the prediction' do
       @wagers.should_receive(:new)
       post_response
     end
-    
+
     it 'should create the response with the posted params' do
       @wagers.should_receive(:new).with(hash_including(:params => 'this is them'))
       post_response({:params => 'this is them'})
     end
-    
+
     it 'should use the current user as the user' do
       user = mock_model(User)
       controller.stub!(:current_user).and_return(user)
       @prediction.responses.should_receive(:new).with(hash_including(:user => user))
       post_response({})
     end
-    
+
     it 'should redirect to the prediction show' do
       post_response
       response.should redirect_to(prediction_path('1'))
     end
-    
+
     describe 'when the params are invalid' do
       before(:each) do
         @wager.stub!(:save => false)
@@ -61,7 +61,7 @@ describe ResponsesController do
       end
     end
   end
-  
+
   describe 'comment preview' do
     before(:each) do
       controller.stub!(:login_required)
@@ -70,29 +70,29 @@ describe ResponsesController do
     def get_preview
       get :preview, :response => { :comment => 'some text' }
     end
-   
+
     it 'should respond to preview action' do
       get_preview
       response.should be_success
     end
-    
+
     it 'should render the preview comment partial' do
       get_preview
       response.should render_template('responses/_preview')
     end
-    
+
     it 'should build a new response on the prediction from the params' do
       Response.should_receive(:new).with('comment' => 'some text').and_return(
         mock_model(Response).as_null_object
       )
       get_preview
     end
-    
+
     it "should not save the comment" do
       Response.stub!(:new).and_return(response = mock_model(Response).as_null_object)
       response.should_not_receive(:save!)
       get_preview
     end
   end
-  
+
 end
