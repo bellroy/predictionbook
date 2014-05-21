7321c5d7d53e73275a905d027817f16110a10d2d
Philip Hazelden <philip.hazelden@gmail.com>
2014-05-21 21:14:20 +0100

    Start adding tests.
    I don't really know how they work, yet.

--- spec/models/credence_question_spec.rb
+++ spec/models/credence_question_spec.rb
@@ -1,5 +1,18 @@
 require 'spec_helper'
 
 describe CredenceQuestion do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'should know right from wrong' do
+    gen = CredenceQuestionGenerator.new(enabled: true, text: "a or b")
+    a0 = CredenceAnswer.new(text: "a", value: "A")
+    a1 = CredenceAnswer.new(text: "b", value: "B")
+    q = CredenceQuestion.new(credence_question_generator: gen,
+                             answer0: a0, answer1: a1,
+                             correct_index: 1)
+    q.answer_correct?(1).should == true
+    q.answer_correct?(0).should == false
+  end
+
+  it 'should create random questions' do
+    pending "Can I test this without a db?"
+  end
 end
