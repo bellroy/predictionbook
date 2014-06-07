fcbb1d764b345dcab0b8950677be4bc0bdedcd6b
Philip Hazelden <philip.hazelden@gmail.com>
2014-06-07 14:32:27 +0100

    Set current_question in after_initialize.

--- app/models/credence_game.rb
+++ app/models/credence_game.rb
@@ -2,9 +2,11 @@ class CredenceGame < ActiveRecord::Base
   belongs_to :user
   belongs_to :current_question, class_name: 'CredenceQuestion'
 
-  def initialize()
-    super
-    new_question
+  after_initialize :ensure_current_question
+  def ensure_current_question()
+    if self.current_question.nil?
+      self.new_question
+    end
   end
 
   def new_question()
