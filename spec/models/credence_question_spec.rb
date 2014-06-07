fc107acfcfb64cb388b0a9fc0a0bb3e6a8658e82
Philip Hazelden <philip.hazelden@gmail.com>
2014-06-07 14:04:01 +0100

    Test credence scores.

--- spec/models/credence_question_spec.rb
+++ spec/models/credence_question_spec.rb
@@ -12,6 +12,28 @@ describe CredenceQuestion do
     q.answer_correct?(0).should == false
   end
 
+  it 'should give correct scores to specific credences' do
+    gen = CredenceQuestionGenerator.new(enabled: true, text: "a or b")
+    a0 = CredenceAnswer.new(text: "a", value: "A")
+    a1 = CredenceAnswer.new(text: "b", value: "B")
+    q = CredenceQuestion.new(credence_question_generator: gen,
+                             answer0: a0, answer1: a1,
+                             correct_index: 1)
+
+    right_scores = { 50 => 0, 51 => 3, 60 => 26, 70 => 49,
+                     80 => 68, 90 => 85, 99 => 99 }
+    wrong_scores = { 50 => 0, 51 => -3, 60 => -32, 70 => -74,
+                     80 => -132, 90 => -232, 99 => -564 }
+
+    right_scores.each do |cred, score|
+      q.score_answer(1, cred).should == [true, score]
+    end
+
+    wrong_scores.each do |cred, score|
+      q.score_answer(0, cred).should == [false, score]
+    end
+  end
+
   it 'should create random questions' do
     pending "Can I test this without a db?"
   end
