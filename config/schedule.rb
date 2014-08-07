d9af05dd8eabdad24ae716aff5acb7902dc1b3d4
Wesley Moore <wes@wezm.net>
2014-08-07 16:43:26 +1000

    Stop deprecation warnings causing an email to be sent every hour

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,9 +5,11 @@
 
 env :PATH, "/usr/local/bin:/usr/bin:/bin"
 
+job_type :bundled_rake, "cd :path && RAILS_ENV=:environment chronic bundle exec rake :task --silent 2>&1 :output | ts"
+
 if environment == "production"
   every 1.hour do
-    rake "send_email_notifications"
+    bundled_rake "send_email_notifications"
   end
 end
 
