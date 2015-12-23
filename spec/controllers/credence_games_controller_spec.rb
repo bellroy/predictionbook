5c59e2ee3791c7d07dc5c47de9fca4b21ab3edf7
Philip Hazelden <philip.hazelden@gmail.com>
2015-12-23 17:02:54 +0000

    Test CredenceGamesController update.

--- spec/controllers/credence_games_controller_spec.rb
+++ spec/controllers/credence_games_controller_spec.rb
@@ -13,7 +13,7 @@ describe CredenceGamesController do
     get :index
   end
 
-  describe 'with an existing game' do
+  describe 'Receiving a question' do
     before(:each) do
       CredenceQuestion.stub(:exists?).and_return(true)
       @game = valid_credence_game
@@ -27,26 +27,107 @@ describe CredenceGamesController do
 
     it 'should assign @game and @question' do
       get :index
-      assigns[:game].should == @game
-      assigns[:question].should == :response
+      expect(assigns[:game]).to eq @game
+      expect(assigns[:question]).to eq :response
     end
 
     it 'should set @show_graph only for certain questions' do
       @game.stub(:num_answered).and_return(10)
       get :index
-      assigns[:show_graph].should == false
+      expect(assigns[:show_graph]).to eq false
 
       @game.stub(:num_answered).and_return(20)
       get :index
-      assigns[:show_graph].should == true
+      expect(assigns[:show_graph]).to eq true
 
       @game.stub(:num_answered).and_return(25)
       get :index
-      assigns[:show_graph].should == false
+      expect(assigns[:show_graph]).to eq false
 
       @game.stub(:num_answered).and_return(30)
       get :index
-      assigns[:show_graph].should == true
+      expect(assigns[:show_graph]).to eq true
+    end
+  end
+
+  describe 'Answering a question' do
+    def check_flash(correct)
+      expect(flash[:correct]).to eq correct
+      expect(flash[:score]).to eq (correct ? 3 : -3)
+      expect(flash[:message]).to be_kind_of(String)
+      expect(flash[:message]).to be_truthy
+    end
+
+    before(:each) do
+      @game = valid_credence_game
+      @question = @game.current_response
+
+      CredenceQuestion.stub(:exists?).and_return(true)
+      CredenceGame.stub(:find_or_create_by_user_id).and_return(@game)
+      @user.stub(:credence_game).and_return(@game)
+
+      @game.stub(:new_question)
+      @game.stub(:save)
+      @question.stub(:save)
+
+      # Can't set id in valid_credence_game_response ("can't mass-assign
+      # protected attributes")
+      @question.stub(:id).and_return(1)
+    end
+
+    it 'should update' do
+      expect(@game).to receive(:new_question)
+      expect(@game).to receive(:save)
+      expect(@question).to receive(:save)
+
+      post :update, :question_id => 1, :answer_index => 0, :credence => 51
+    end
+
+    it 'should not update when the question id is wrong' do
+      expect(@game).not_to receive(:new_question)
+      expect(@game).not_to receive(:save)
+      expect(@question).not_to receive(:save)
+
+      CredenceGameResponse.stub(:find).and_return(@question)
+      @question.stub(:given_answer).and_return(0)
+      @question.stub(:answer_credence).and_return(51)
+      post :update, :question_id => 2, :answer_index => 0, :credence => 51
+
+      check_flash(true)
+    end
+
+    it 'should set the flash correctly' do
+      post :update, :question_id => 1, :answer_index => 0, :credence => 51
+      check_flash(true)
+
+      post :update, :question_id => 1, :answer_index => 1, :credence => 51
+      check_flash(false)
+
+      CredenceGameResponse.stub(:find).and_return(@question)
+      @question.stub(:answer_credence).and_return(51)
+
+      @question.stub(:given_answer).and_return(0)
+
+      post :update, :question_id => 2, :answer_index => 0, :credence => 70
+      check_flash(true)
+
+      post :update, :question_id => 2, :answer_index => 1, :credence => 70
+      check_flash(true)
+
+      @question.stub(:given_answer).and_return(1)
+
+      post :update, :question_id => 2, :answer_index => 0, :credence => 70
+      check_flash(false)
+
+      post :update, :question_id => 2, :answer_index => 1, :credence => 70
+      check_flash(false)
+    end
+
+    it 'should give a helpful error for credences outside [1, 99]' do
+      # Not critical - the model rejects them, so they can't be used to cheat,
+      # and there's no supported way to pass them.
+      pending 'Not yet implemented'
+      raise 'Not yet implemented'
     end
   end
 end
