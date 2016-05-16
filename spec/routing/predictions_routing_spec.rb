7498e0024c8364e684f27828374660bba5acc123
Michael Webb <moggyboy@gmail.com>
2016-05-16 23:32:04 +1000

    UsersController: get specs passing.

--- spec/routing/predictions_routing_spec.rb
+++ spec/routing/predictions_routing_spec.rb
@@ -1,41 +1,43 @@
 require 'spec_helper'
 
-describe "routing to predictions" do
-
+describe 'routing to predictions' do
   it 'maps  GET / to home action' do
-    {:get=> '/' }.should route_to(:controller => 'predictions', :action => 'home')
+    expect(get: '/').to route_to(controller: 'predictions', action: 'home')
   end
 
   it 'maps  /predictions to "index" action' do
-    {:get => '/predictions'}.should route_to(:controller => 'predictions', :action => 'index')
+    expect(get: '/predictions').to route_to(controller: 'predictions', action: 'index')
   end
 
   it 'maps  GET to /predictions/new to "new" action' do
-    {:get => '/predictions/new'}.should route_to(:controller => 'predictions', :action => 'new')
+    expect(get: '/predictions/new').to route_to(controller: 'predictions', action: 'new')
   end
 
   it 'maps  GET to / to "index" action' do
-    {:get => '/'}.should route_to(:controller => 'predictions', :action => 'home')
+    expect(get: '/').to route_to(controller: 'predictions', action: 'home')
   end
 
   it 'maps  GET /preditions/:id to show' do
-    {:get => '/predictions/1'}.should route_to(:controller => 'predictions', :action => 'show', :id => '1')
+    expect(get: '/predictions/1').to route_to(controller: 'predictions', action: 'show', id: '1')
   end
 
   it 'maps  POST to /predictions/:id/judge to "judge" action' do
-    {:post => '/predictions/1/judge'}.should route_to(:controller => 'predictions', :action => 'judge', :id => '1')
+    expect(post: '/predictions/1/judge').to route_to(
+      controller: 'predictions', action: 'judge', id: '1'
+    )
   end
 
   it 'maps  POST to /predictions/:id/withdraw to update action' do
-    {:post => '/predictions/1/withdraw'}.should route_to(:controller => 'predictions', :action => 'withdraw', :id => '1')
+    expect(post: '/predictions/1/withdraw').to route_to(
+      controller: 'predictions', action: 'withdraw', id: '1'
+    )
   end
 
   it 'maps  GET /preditions/:id to show' do
-    {:get => '/predictions/1'}.should route_to(:controller => 'predictions', :action => 'show', :id => '1')
+    expect(get: '/predictions/1').to route_to(controller: 'predictions', action: 'show', id: '1')
   end
 
   it 'maps  POST to /predictions to "create" action' do
-    {:post => '/predictions'}.should route_to(:controller => 'predictions', :action => 'create')
+    expect(post: '/predictions').to route_to(controller: 'predictions', action: 'create')
   end
-
 end
--- spec/routing/responses_routing_spec.rb
+++ spec/routing/responses_routing_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe 'routing to responses' do
   it 'should route responses/preview to preview action' do
-    {:get => '/responses/preview'}.should route_to(:controller=> 'responses', :action=> 'preview')
+    expect(get: '/responses/preview').to route_to(controller: 'responses', action: 'preview')
   end
 end
