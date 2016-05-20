fbb67ceaaa94bedbc957e4d5f09c692b036c802c
Michael Webb <moggyboy@gmail.com>
2016-05-20 12:14:35 +1000

    MarkupHelperSpec: cleanup shoulds

--- spec/helpers/markup_helper_spec.rb
+++ spec/helpers/markup_helper_spec.rb
@@ -6,18 +6,18 @@ describe MarkupHelper do
   describe '#confidence_and_count' do
     it 'returns the number of wagers of a prediction' do
       prediction = double(Prediction, wager_count: 20).as_null_object
-      confidence_and_count(prediction).should =~ /20/
+      expect(confidence_and_count(prediction)).to match(/20/)
     end
 
     it 'returns the mean confidence of a prediction' do
       prediction = double(Prediction, mean_confidence: '13').as_null_object
-      confidence_and_count(prediction).should =~ /13/
+      expect(confidence_and_count(prediction)).to match(/13/)
     end
   end
 
   describe 'certainty_heading' do
     it 'should not add % to the end of the heading' do
-      certainty_heading('60').should == '60'
+      expect(certainty_heading('60')).to eq '60'
     end
 
     describe '100% easter egg' do
@@ -25,22 +25,23 @@ describe MarkupHelper do
         @egg = certainty_heading('100')
       end
       it 'should add wiki almost surely article link' do
-        @egg.should have_link('Almost surely', href: 'http://en.wikipedia.org/wiki/Almost_surely')
+        link = 'http://en.wikipedia.org/wiki/Almost_surely'
+        expect(@egg).to have_link('Almost surely', href: link)
       end
     end
   end
 
   describe 'css classes helper' do
     it 'should join args together with spaces' do
-      classes('one', 'two').should == 'one two'
+      expect(classes('one', 'two')).to eq 'one two'
     end
 
     it 'filters out nils' do
-      classes('test', nil, 'val').should == 'test val'
+      expect(classes('test', nil, 'val')).to eq 'test val'
     end
 
     it 'should flatten lists in the argument list' do
-      classes('test', %w(two three)).should == 'test two three'
+      expect(classes('test', %w[two three])).to eq 'test two three'
     end
   end
 end
