082012f9286dd696b1394be22232703ad23df263
Michael Webb <moggyboy@gmail.com>
2016-05-22 21:16:00 +1000

    Cleaned up spec.

--- spec/views/predictions/index.html.erb_spec.rb
+++ spec/views/predictions/index.html.erb_spec.rb
@@ -4,13 +4,13 @@ describe 'predictions/index.html.erb' do
   before do
     assign(:predictions, [])
     assign(:statistics, Statistics.new)
-    view.stub(:show_statistics?).and_return(false)
-    view.stub(:current_user).and_return User.new
+    allow(view).to receive(:show_statistics?).and_return(false)
+    allow(view).to receive(:current_user).and_return User.new
   end
 
   it 'renders without errors' do
     render
-    rendered.should_not be_blank
+    expect(rendered).not_to be_blank
   end
 
   it 'renders predictions' do
@@ -18,6 +18,6 @@ describe 'predictions/index.html.erb' do
     assign(:predictions, [prediction])
 
     render
-    rendered.should have_selector("a[href='#{prediction_path(prediction)}']")
+    expect(rendered).to have_selector("a[href='#{prediction_path(prediction)}']")
   end
 end
