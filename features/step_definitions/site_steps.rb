0cad2e98a75b47330f2c447b9ec672a2882ad675
Michael Webb <moggyboy@gmail.com>
2016-05-22 22:35:31 +1000

    Remove old should syntax.

--- features/step_definitions/site_steps.rb
+++ features/step_definitions/site_steps.rb
@@ -1,5 +1,5 @@
 Given /^I am logged in$/ do
-  @user = FactoryGirl.create(:user, login: 'buzzo', password: 'testing')
+  @user = FactoryGirl.create(:user, login: 'buzzo')
   visit new_user_session_path
   fill_in 'Login', with: @user.login
   fill_in 'Password', with: @user.password
