f9009d179cf159b803af1f8aeccd9ab6ec741b49
Michael Webb <moggyboy@gmail.com>
2016-05-22 21:50:58 +1000

    Clean up shoulds and stubs.

--- spec/views/predictions/new.html.erb_spec.rb
+++ spec/views/predictions/new.html.erb_spec.rb
@@ -2,85 +2,83 @@ require 'spec_helper'
 
 describe 'predictions/new' do
   before(:each) do
-    @prediction = mock_model(Prediction, persisted?: false, errors: {}).as_null_object
+    @prediction = FactoryGirl.build(:prediction)
     assign(:prediction, @prediction)
-    view.stub(:logged_in?).and_return(true)
-    view.stub(:user_statistics_cache_key).and_return 'stats'
-    view.stub(:statistics).and_return(Statistics.new)
-    @user = mock_model(User, has_email?: false)
-    @user.stub(:to_param).and_return 'username'
-    view.stub(:current_user).and_return(@user)
+    allow(view).to receive(:user_statistics_cache_key).and_return 'stats'
+    allow(view).to receive(:statistics).and_return(Statistics.new)
+    @user = instance_double(User, has_email?: false)
+    allow(@user).to receive(:to_param).and_return 'username'
+    allow(view).to receive(:current_user).and_return(@user)
   end
 
-  it 'should look up the user cache key for the current user' do
-    view.should_receive(:user_statistics_cache_key).with(@user)
+  it 'looks up the user cache key for the current user' do
+    expect(view).to receive(:user_statistics_cache_key).with(@user)
     render
-  end
-
-  it 'should cache a fragment for the statistics partial' do
-    lambda do
-      render
-    end.should cache_fragment('views/stats')
   end
 
   it 'has a form that POSTs to the prediction collection' do
     render
-    rendered.should have_selector("form[method='post'][action='/predictions']")
+    expect(rendered).to have_selector("form[method='post'][action='/predictions']")
   end
 
   it 'has a hidden field with the predictions UUID' do
-    @prediction.stub(:uuid).and_return('0d027d60-7b04-11dd-92d8-001f5b80f5b2')
+    expect(@prediction).to receive(:uuid).and_return('0d027d60-7b04-11dd-92d8-001f5b80f5b2')
     render
-    rendered.should have_selector("input[type='hidden'][name='prediction[uuid]'][value='0d027d60-7b04-11dd-92d8-001f5b80f5b2']")
+    selector =
+      "input[type='hidden'][name='prediction[uuid]'][value='0d027d60-7b04-11dd-92d8-001f5b80f5b2']"
+    expect(rendered).to have_selector(selector)
   end
 
   it 'has an input field for prediction description' do
     render
-    rendered.should have_selector("textarea[name='prediction[description]']")
+    expect(rendered).to have_selector("textarea[name='prediction[description]']")
   end
 
   it 'has an input field for the initial confidence' do
     render
-    rendered.should have_selector("input[type='text'][name='prediction[initial_confidence]']")
+    expect(rendered).to have_selector("input[type='text'][name='prediction[initial_confidence]']")
   end
 
   describe '(check box for the notify creator)' do
     it 'is present and checked when user has an email' do
-      @prediction.stub(:notify_creator).and_return true
+      expect(@prediction).to receive(:notify_creator).and_return true
       render
-      rendered.should have_checked_field('prediction[notify_creator]')
+      expect(rendered).to have_checked_field('prediction[notify_creator]')
     end
+
     it 'is unchecked if user does not have email' do
-      @prediction.stub(:notify_creator).and_return false
+      expect(@prediction).to receive(:notify_creator).and_return false
       render
-      rendered.should_not have_checked_field('prediction[notify_creator]')
+      expect(rendered).to_not have_checked_field('prediction[notify_creator]')
     end
   end
 
   describe '(check box for private)' do
     it 'is present' do
       render
-      rendered.should have_field('prediction[private]')
+      expect(rendered).to have_field('prediction[private]')
     end
+
     it 'is checked when user private_default is true' do
-      @prediction.stub(:private).and_return true
+      expect(@prediction).to receive(:private).and_return true
       render
-      rendered.should have_checked_field('prediction[private]')
+      expect(rendered).to have_checked_field('prediction[private]')
     end
-    it 'should not be checked when user private_default is false' do
-      @prediction.stub(:private).and_return false
+
+    it 'is not checked when user private_default is false' do
+      expect(@prediction).to receive(:private).and_return false
       render
-      rendered.should_not have_checked_field('prediction[private]')
+      expect(rendered).to_not have_checked_field('prediction[private]')
     end
   end
 
   it 'has an input field for the result' do
     render
-    rendered.should have_selector("input[type='text'][name='prediction[deadline_text]']")
+    expect(rendered).to have_selector("input[type='text'][name='prediction[deadline_text]']")
   end
 
   it 'has a submit button' do
     render
-    rendered.should have_css('input[type="submit"]')
+    expect(rendered).to have_css('input[type="submit"]')
   end
 end
