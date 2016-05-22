c13714601f4ae8779b01b79af90d757c9c95a3a3
Michael Webb <moggyboy@gmail.com>
2016-05-22 14:12:46 +1000

    Time Core Ext spec: cleaned up.

--- spec/lib/time_core_ext_spec.rb
+++ spec/lib/time_core_ext_spec.rb
@@ -3,24 +3,21 @@ require 'spec_helper'
 describe 'Time core extensions' do
   describe 'noon' do
     it 'sets Time instance to 12:00:00' do
-      Time.utc(2000,11,23,10,50,44).noon.should == Time.utc(2000,11,23,12,00,00)
+      expect(Time.utc(2000, 11, 23, 10, 50, 44).noon).to eq Time.utc(2000, 11, 23, 12, 00, 00)
     end
   end
 end
 
 describe 'Chronic regression test' do
-
   describe 'crash on "100 years from now"' do
-    it 'should not raise an exception' do
+    it 'does not raise an exception' do
       expect { Chronic.parse('100 years from now') }.not_to raise_error
     end
 
     it 'returns the date' do
-      now = Time.now
+      now = Time.zone.now
       future = now.utc + 100.years
-      Time.stub(:now).and_return(now)
-      Chronic.parse("100 years from now").to_s(:db).should == future.to_s(:db)
+      expect(Chronic.parse('100 years from now').to_s(:db)).to eq future.to_s(:db)
     end
   end
-
 end
