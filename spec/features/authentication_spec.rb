e35f24385fef12c678e161c079746dfd47e2c897
Michael Webb <moggyboy@gmail.com>
2016-05-25 19:59:50 +1000

    Add authentication spec.

---
+++ spec/features/authentication_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+feature 'authentication' do
+  let!(:user) { FactoryGirl.create(:user, name: 'Bob', login: 'login') }
+
+  scenario 'user logs in and logs out again' do
+    visit root_path
+    within 'ul#user-links' do
+      click_link 'Login'
+    end
+    fill_in 'user_login', with: 'login'
+    fill_in 'user_password', with: 'password'
+    click_button 'Log in'
+    expect(page).to have_content 'Hello, Bob'
+    within 'ul#user-links' do
+      click_link 'Logout'
+    end
+    expect(page).to have_link 'Login'
+  end
+end
