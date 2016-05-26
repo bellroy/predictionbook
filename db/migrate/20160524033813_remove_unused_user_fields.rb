d9149c4ef3b857749bbe61633391e4157225ced0
Michael Webb <moggyboy@gmail.com>
2016-05-26 12:39:21 +1000

    Undo removal of old password fields.

--- db/migrate/20160524033813_remove_unused_user_fields.rb
+++
@@ -1,6 +0,0 @@
-class RemoveUnusedUserFields < ActiveRecord::Migration
-  def change
-    remove_column :users, :crypted_password, :string, limit: 40
-    remove_column :users, :salt, :string, limit: 40
-  end
-end
