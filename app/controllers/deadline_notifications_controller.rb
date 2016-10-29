b79d1f7eb0c5cd36e88cb82cebd7ad6627deb253
Andrew Pratley <pratley.andrew@gmail.com>
2016-10-30 02:02:28 +1100

    removed pagination from email notifications page

--- app/controllers/deadline_notifications_controller.rb
+++ app/controllers/deadline_notifications_controller.rb
@@ -3,13 +3,9 @@ class DeadlineNotificationsController < NotificationsController
     @user = User.find_by_login(params[:user_id]) || User.find_by_id(params[:user_id])
 
     @pending = @user.deadline_notifications.sendable.sort
-    @pending = Kaminari.paginate_array(@pending).page(params[:page]).per(20)
     @waiting = @user.deadline_notifications.unsent.enabled.unknown.sort
-    @waiting = Kaminari.paginate_array(@waiting).page(params[:page]).per(20)
     @known = @user.deadline_notifications.unsent.enabled.known.rsort(:judged_at)
-    @known = Kaminari.paginate_array(@known).page(params[:page]).per(20)
     @sent = @user.deadline_notifications.sent.rsort(:deadline)
-    @sent = Kaminari.paginate_array(@sent).page(params[:page]).per(20)
   end
 
   def notification_type
