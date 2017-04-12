45d183462a73527f6e3a46cfdf5eed4fe463f9e6
Michael Webb <moggyboy@gmail.com>
2017-04-12 16:38:17 +1000

    Add helper for generating visibility options

--- app/models/visibility.rb
+++ app/models/visibility.rb
@@ -1,3 +1,11 @@
 module Visibility
   VALUES = { visible_to_everyone: 0, visible_to_creator: 1 }.freeze
+
+  def self.select_options_html(value)
+    VALUES.keys.map do |key|
+      key_string = key.to_s
+      label = key_string.humanize
+      "<option value=\"#{key}\" #{value == key_string ? 'selected' : ''}>#{label}</option>"
+    end.join
+  end
 end
