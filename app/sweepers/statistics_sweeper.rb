cc73c6a235b6e602b034ddee8d8dee64b73c199e
Jayson Virissimo <jayson.virissimo@asu.edu>
2017-04-12 23:31:16 -0700

    Reset user scores after prediction judgment

--- app/sweepers/statistics_sweeper.rb
+++ app/sweepers/statistics_sweeper.rb
@@ -4,8 +4,11 @@ class StatisticsSweeper < ActionController::Caching::Sweeper
   include CacheKeys
 
   def after_create(judgement)
-    judgement.prediction.wagers.collect(&:user).each do |user|
+    associated_users = judgement.prediction.wagers.collect(&:user).uniq
+
+    associated_users.each do |user|
       Rails.cache.clear(user_statistics_cache_key(user))
+      user.reset_score
     end
 
     Rails.cache.clear(global_statistics_cache_key)
