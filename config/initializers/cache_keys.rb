bf7a42c4c3b9e72edaa8bf477abe02d72d12daa6
Michael Webb <moggyboy@gmail.com>
2017-07-06 16:59:22 +1000

    Group calibration leaderboard

--- config/initializers/cache_keys.rb
+++ config/initializers/cache_keys.rb
@@ -15,6 +15,10 @@ module CacheKeys
     "group_calibration_scores_partial-#{group.to_param}"
   end
 
+  def group_leaderboard_cache_key(group)
+    "group_leaderboard_partial-#{group.to_param}"
+  end
+
   def global_statistics_cache_key
       "statistics_partial"
   end
