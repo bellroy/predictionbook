71b4777c61e61c85825fb334b4afe9bce937a3a4
Michael Webb <moggyboy@gmail.com>
2018-11-22 16:44:24 +1100

    Remove unneccesary class definition from migration

--- db/migrate/20120507034756_add_admin_flag_to_users.rb
+++ db/migrate/20120507034756_add_admin_flag_to_users.rb
@@ -1,9 +1,5 @@
-class User < ActiveRecord::Base
-end
-
 class AddAdminFlagToUsers < ActiveRecord::Migration[4.2]
   def change
     add_column :users, :admin, :boolean, null: false, default: false
   end
 end
-
