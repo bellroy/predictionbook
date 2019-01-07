1bc4bf3022779994f99c75751e5cca286b06411f
Bruno Parga <brunoparga@gmail.com>
2019-01-07 17:27:42 -0200

    Test when user wants to filter their own predictions

--- app/queries/prediction_filter.rb
+++ app/queries/prediction_filter.rb
@@ -5,6 +5,10 @@ module PredictionFilter
     predictions = user.predictions
     predictions = predictions.visible_to_everyone unless current_user == user
 
+    # Since these methods from Prediction only filter visible_to_everyone
+    # predictions, self.filter won't include private predictions.
+    # TODO: improve Prediction's methods
+
     predictions =
       case filter
       when 'judged' then predictions.judged
