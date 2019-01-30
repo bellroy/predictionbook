bf783d91b2523e139a90eabcb909cf3a1228208f
Bruno Parga <brunoparga@gmail.com>
2019-01-30 14:23:18 -0200

    Install bullet gem
    Profile SQL queries to avoid N+1 and useless eager loading.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,13 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: "localhost" }
 
   config.middleware.delete Rack::Lock
+
+  config.after_initialize do
+    Bullet.enable = true
+    Bullet.alert = true
+    Bullet.bullet_logger = true
+    Bullet.console = true
+    Bullet.rails_logger = true
+    Bullet.add_footer = true
+  end
 end
