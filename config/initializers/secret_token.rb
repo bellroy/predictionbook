17a9588ed2cd0a52085a0a8e900355613d3c7321
Bruno Parga <brunoparga@gmail.com>
2019-08-22 03:03:00 -0300

    Remove deprecation warning

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,5 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-PredictionBook::Application.config.secret_token = PredictionBook::Application.config.credentials.session_secret
-PredictionBook::Application.config.secret_key_base = PredictionBook::Application.config.secret_token
+PredictionBook::Application.config.secret_key_base = PredictionBook::Application.config.credentials.session_secret
