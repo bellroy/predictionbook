55c6f054c5b7ee1a1e13fc6791acf238d4f11e8e
Jayson Virissimo <jayson.virissimo@asu.edu>
2021-12-08 22:19:56 -0700

    Introduce predictions query object

--- app/controllers/api/my_predictions_controller.rb
+++ app/controllers/api/my_predictions_controller.rb
@@ -2,10 +2,6 @@
 
 module Api
   class MyPredictionsController < AuthorisedController
-    MAXIMUM_PAGE_SIZE = 1000
-    DEFAULT_PAGE_SIZE = 100
-    DEFAULT_PAGE = 1
-
     def index
       render json: my_predictions_hash
     end
@@ -24,17 +20,53 @@ module Api
     end
 
     def predictions
-      page_size = params[:page_size].to_i
-      page_size = DEFAULT_PAGE_SIZE unless (1..MAXIMUM_PAGE_SIZE).cover?(page_size)
-      page = params[:page].to_i
-      page = DEFAULT_PAGE unless page.positive?
-      @user
-        .predictions
-        .not_withdrawn
-        .includes(Prediction::DEFAULT_INCLUDES)
-        .order(created_at: :desc)
-        .page(page)
-        .per(page_size)
+      PredictionsQuery.new(
+        user: @user,
+        page: params[:page].to_i,
+        page_size: params[:page_size].to_i
+      ).call
+    end
+
+    class PredictionsQuery
+      DEFAULT_PAGE = 1
+      DEFAULT_PAGE_SIZE = 100
+      MAXIMUM_PAGE_SIZE = 1000
+
+      def initialize(user:, page: DEFAULT_PAGE, page_size: DEFAULT_PAGE_SIZE)
+        @page = page
+        @page_size = page_size
+        @user = user
+      end
+
+      def call
+        user
+          .predictions
+          .not_withdrawn
+          .includes(Prediction::DEFAULT_INCLUDES)
+          .order(created_at: :desc)
+          .page(page)
+          .per(page_size)
+      end
+
+      private
+
+      attr_reader :user
+
+      def page
+        if @page.positive?
+          @page
+        else
+          DEFAULT_PAGE
+        end
+      end
+
+      def page_size
+        if (1..MAXIMUM_PAGE_SIZE).cover?(@page_size)
+          @page_size
+        else
+          DEFAULT_PAGE_SIZE
+        end
+      end
     end
   end
 end
