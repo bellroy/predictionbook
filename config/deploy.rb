2e0b119aee57849584895c3d9d9da8a3c0a84827
Chris D'Aloisio <chris.daloisio@bellroy.com>
2023-03-09 22:05:58 +1100

    remove more redundant cap tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,5 @@
 set :repo_url,            'git@github.com:bellroy/predictionbook.git'
-set :secrets_repository,  'git@git.trikeapps.com:settings/predictionbook.git'
 set :linked_dirs,         ['public/assets', 'log']
-set :symlinked_configs,   ['database.yml', "credentials/#{fetch(:stage)}.key"]
 set :engine,              'passenger'
 set :deploy_to,           -> { "/srv/www/#{fetch(:application)}" }
 set :format,              :pretty
@@ -11,15 +9,6 @@ set :whenever_identifier, -> { "#{fetch(:application)}_#{fetch(:stage)}" }
 set :slack_webhook_urls,  [ENV['SLACK_WEBHOOK_URL']]
 
 namespace :deploy do
-  desc 'Create a symlink in the release path to all _symlinked_configs_'
-  task :symlink_configs do
-    on roles(:app) do
-      fetch(:symlinked_configs).each do |file|
-        execute "ln -sf #{shared_path}/config/#{file} #{release_path}/config/#{file}"
-      end
-    end
-  end
-
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
@@ -28,6 +17,5 @@ namespace :deploy do
   end
 
   after :publishing, :restart
-  after 'deploy:symlink:linked_dirs', :symlink_configs
   after 'deploy:cleanup', 'deploy:tag'
 end
