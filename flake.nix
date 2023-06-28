1e579b4765c7e3b1aac11def5b7083f289e0d1d8
Chris D'Aloisio <chris.daloisio@bellroy.com>
2023-06-28 22:44:25 +1000

    chore: migrate from niv to flakes for nix management

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "PredictionBook";
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-23.05-darwin";
+  };
+  outputs = inputs:
+  inputs.flake-utils.lib.eachSystem [
+    "x86_64-linux"
+    "x86_64-darwin"
+  ] (system:
+  let
+    pkgs = import inputs.nixpkgs { inherit system; };
+    ruby = pkgs.ruby_3_1;
+    bundler = pkgs.bundler.override { inherit ruby; };
+    developmentInputs = with pkgs; [
+      bundler
+      libnotify
+      pkg-config
+      postgresql_12
+      readline
+      ruby
+      zlib
+    ]
+    ++ (lib.optionals stdenv.hostPlatform.isDarwin [ libiconv darwin.apple_sdk.frameworks.CoreServices ]);
+  in
+  {
+    devShells.default= pkgs.mkShell {
+      name = "development";
+      buildInputs = developmentInputs;
+      shellHook = ''
+        bundle config --local path "$PWD/vendor/bundle"
+        bundle config --local build.sassc --disable-lto
+        ruby -e "puts RUBY_VERSION" > "$PWD/.ruby-version"
+      '';
+    };
+  });
+}
+
